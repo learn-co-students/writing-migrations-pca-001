056c1cdb8f510077fe0e3d7b6f68f335b71d448b
Liam Tinney <ltinney@powerhrg.com>
2020-05-29 10:09:26 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,11 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+require 'pry'
+
+class CreateStudents < ActiveRecord::Migration[4.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :grade
+      t.string :birthdate
+    end
+  end
 end
---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
+  def change
+  end
+end
